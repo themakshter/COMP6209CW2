654f3a101bed23455c5522800b75f007545aff5f
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-13 23:14:35 +0100

    Add expressions and main program for part 1 - need to make it compile + run

--- part1/expressions.h
+++ part1/expressions.h
@@ -1,6 +1,78 @@
-#ifndef EXPRESSIOSN_H_INCLUDED
-#define EXPRESSIOSN_H_INCLUDED
+#ifndef EXPRESSIONS_H_INCLUDED
+#define EXPRESSIONS_H_INCLUDED
+
+#include <stdio.h>
+
+using namespace std;
+
+class divideByZeroException:
+    public exception{
+    virtual const char* what() const throw()
+    {
+        return "Error! Cannot divide by zero!";
+    }
+
+};
+
+
+template <int i>
+class Value{
+public:
+    static int eval(int x){
+        return x;
+    }
+};
+
+class Variable{
+public:
+    static int eval(int x){
+        return x;
+    }
+};
+
+template<class a>
+class Expression{
+    static int eval(int x){
+        return a::eval(x);
+    }
+};
+
+template<class a,class b>
+class Add{
+public:
+        static int eval(int x){
+            return a::eval(x) + b::eval(x);
+        }
+};
+
+template<class a, class b>
+class Subtract{
+public:
+    static int eval(int x){
+        return a::eval(x) - b::eval(x);
+    }
+};
+
+template<class a,class b>
+class Multiply{
+public:
+    static int eval(int x){
+        return a::eval(x) * b::eval(x);
+    }
+};
+
+template<class a,class b>
+class Divide{
+public:
+    static int eval(int x){
+        if(b::eval(x) == 0){
+            throw divideByZeroException();
+        }else{
+            return a::eval(x) / b::eval(x);
+        }
+    }
+};
 
 
 
-#endif // EXPRESSIOSN_H_INCLUDED
+#endif // EXPRESSIONS_H_INCLUDED
--- part1/main.cpp
+++ part1/main.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
-
-using namespace std;
+#include "expressions.h"
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    //x + (x - 2) * (x - 3)
+    typedef Expression<Add<Variable,Multiply<Subtract<Variable,Value<2>>,Subtract<Variable,Value<3>>>>> exp
+            Expression<Add<Variable,Multiply<Subtract<Variable,Value<2>>,Subtract<Variable,Value<3>>>>>
+    try{
+        printf("Value of the expression with x=5 is %d\n",exp.eval(5));
+    }catch(exception &e){
+        cout << e.what() << endl
+    }
     return 0;
 }
