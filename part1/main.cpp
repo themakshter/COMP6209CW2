4867c43ca855f22833e727b02a2e9ae3db069a7b
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-05-14 00:21:22 +0100

    Fixing things

--- part1/main.cpp
+++ part1/main.cpp
@@ -4,11 +4,12 @@
 int main()
 {
     //x + (x - 2) * (x - 3)
-    typedef Expression<Add<Variable,Multiply<Subtract<Variable,Value<2>>,Subtract<Variable,Value<3>>>>> exp
+    typedef Expression<Add<Variable,Multiply<Subtract<Variable,Value<2>>,Subtract<Variable,Value<3>>>>> expr;
+
     try{
-        printf("Value of the expression with x=5 is %d\n",exp::eval(5));
+        printf("Value of the expression with x=5 is %d\n",expr::eval(5));
     }catch(exception &e){
-        cout << e.what() << endl
+        cout << e.what() << endl;
     }
     return 0;
 }
